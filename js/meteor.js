5fee02c747e456224559be7e184fe97eb443c430
Helton Silveira <helton.souza@egis-brasil.com.br>
2017-06-12 12:20:43 -0300

    meteor inserido na tela

---
+++ js/meteor.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function Meteor(context, imagem) {
+    this.context = context;
+    this.imagem = imagem;
+    this.x = 0
+    this.y = 0
+    this.velocidade = 0
+}
+
+Meteor.prototype = {
+    atualizar: function () {
+        this.y += this.velocidade;
+    },
+    desenhar: function () {
+        var ctx = this.context;
+        var img = this.imagem;
+        ctx.drawImage(img, this.x, this.y, img.width, img.height);
+    }
+}
--- js/nave.js
+++ js/nave.js
@@ -12,10 +12,10 @@ Nave.prototype = {
         if (this.teclado.pressionada(SETA_DIREITA) && this.x < this.context.canvas.width - this.imagem.width) this.x += this.velocidade;
         if (this.teclado.pressionada(SETA_ACIMA) && this.y > 0) this.y -= this.velocidade;
         if (this.teclado.pressionada(SETA_ABAIXO) && this.y < this.context.canvas.height - this.imagem.height) this.y += this.velocidade;
-
     },
     desenhar: function () {
-        this.context.drawImage(this.imagem, this.x, this.y, this.imagem.width, this.imagem.height);
+        this.context.drawImage(this.imagem, this.x, this.y, this.imagem.width,
+            this.imagem.height);
 
     },
     atirar: function () {
